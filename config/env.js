3fd35f389365d107432827163159b6cc24e28d45
Nithish <nithishken@gmail.com>
2025-08-16 14:56:26 +0530

    commit

--- config/env.js
+++ config/env.js
@@ -8,4 +8,9 @@ export const{
     JWT_EXPIRES_IN,
     ARCJET_ENV,
     ARCJET_KEY,
+    QSTASH_URL,
+    QSTASH_TOKEN,
+    SERVER_URL
+
+
 }=process.env
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async(req,res,next) => {
     try {
@@ -6,15 +8,34 @@ export const createSubscription = async(req,res,next) => {
             ... req.body,
             user: req.user._id
         });
-        res.status(200).json({success:true,data:subscription});
+
+        const { workflowRunId } = await workflowClient.trigger({
+            url:`${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body:{
+                subscriptionId:subscription.id
+            },
+            headers:{
+                'content-type':'application/json',
+            },
+            retries:0
+        });
+
+        res.status(201).json({success:true,data:{subscription,workflowRunId}});
 
     } catch (error) {
         next(error);
     }
 }
 
-export const getUserSubcriptions = async (req,res,next) => {
+export const getUserSubscriptions = async (req,res,next) => {
     try {
+        if(req.user.id!==req.params.id){
+            const error = new Error('You are not owner of this account');
+            error.statusCode=401;
+            throw error;
+        }
+        const subscriptions=await Subscription.find({ user: req.user._id });
+        res.status(200).json({ success: true, data: subscriptions });
         
     } catch (error) {
         next(error);
